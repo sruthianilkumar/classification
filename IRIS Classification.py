e18c6a259ad3adfa0392cf7e0c0de8d8b6b92a1e
sak <52043483+sruthianilkumar@users.noreply.github.com>
2022-11-18 16:28:08 +0530

    Update IRIS Classification.py

--- IRIS Classification.py
+++ IRIS Classification.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
 
 
 #Importing libraries
@@ -12,31 +8,17 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 
-# In[3]:
+#Read dataset
 
+from sklearn.datasets import load_iris
+data = load_iris()
 
-data = pd.read_csv('iris.csv')
-
-
-# In[4]:
-
-
+#print dataset
 data.head()
-
-
-# In[5]:
-
 
 data.info()
 
-
-# In[6]:
-
-
 data['Species'].value_counts()
-
-
-# In[7]:
 
 
 import seaborn as sns
@@ -44,29 +26,16 @@ sns.pairplot(data,hue='Species')
 plt.show()
 
 
-# In[11]:
-
-
 x = data.drop(['Id', 'Species'], axis=1)
 y = data['Species']
 print(x.head())
-
-
-# In[14]:
 
 
 features=list(data.columns)
 features.remove('Id')
 features.remove('Species')
 
-
-# In[15]:
-
-
 features
-
-
-# In[16]:
 
 
 #splitting of data into training and testing
@@ -76,31 +45,14 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 
-# In[17]:
-
 
 x_train.shape
 
-
-# In[18]:
-
-
 x_test.shape
-
-
-# In[19]:
-
 
 y_train.shape
 
-
-# In[20]:
-
-
 y_test.shape
-
-
-# In[21]:
 
 
 from sklearn.preprocessing import StandardScaler
@@ -113,54 +65,33 @@ x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
 
 
-# In[22]:
-
-
-#x_train
-
-
-# In[23]:
-
+#SVM
 
 from sklearn.svm import SVC
 from sklearn import metrics
-
-
-# In[24]:
-
 
 model = SVC()
 model.fit(x_train,y_train)
 y_pred = model.predict(x_test)
 
-
-# In[25]:
-
-
 y_pred
-
-
-# In[26]:
-
 
 print("training accuracy :", model.score(x_train, y_train))
 print("testing accuracy :", model.score(x_test, y_test))
+
+
+#CONFUSION MATRIX
 
 from sklearn.metrics import confusion_matrix
 
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 
-
-# In[27]:
-
+#ACCURACY
 
 from sklearn.metrics import accuracy_score
 
 print("Accuracy: " ,accuracy_score(y_test, y_pred)*100)
-
-
-# In[28]:
 
 
 # Importing classification report
@@ -170,23 +101,12 @@ from sklearn.metrics import classification_report
 print(classification_report(y_test, y_pred))
 
 
-# In[32]:
-
-
 #model = SVC()
 
 new=np.array([[6.9,3.1,4.9,1.5], [5.9,3,4.2,1.5], [6,2.2,5,1.5]])
 prediction=model.predict(new)
 print("prediction: {}".format(prediction))
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
